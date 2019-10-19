c0d5b111e073db5733d53a73bed81afbbe99f81b
John Fraschilla <jfraschilla@outlook.com>
2019-10-18 20:03:48 -0700

    Added Unity Container Examples

---
+++ UnityContainer-Samples/UnityFactoryExample/UnityFactoryExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using Unity;
+using Abmes.Unity.TypedFactories;
+
+
+namespace UnityFactoryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var container = new UnityContainer();
+            container.RegisterTypedFactory<IMyFactory>().ForConcreteType<Foo>();
+            container.RegisterType<IFoo, Foo>();
+            var factory = container.Resolve<IMyFactory>();
+            var foo = factory.Create("some name");
+            Assert.AreEqual("some name", foo.Name);
+        }
+    }
+
+    public interface IMyFactory
+    {
+        IFoo Create(string name);
+    }
+
+    public interface IFoo
+    {
+        string Name { get; }
+    }
+
+    public class Foo : IFoo
+    {
+        public string Name { get; set; }
+        public Foo(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ UnityContainer-Samples/UnityFactoryInjection/UnityFactoryInjection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Unity;
+
+namespace UnityFactoryInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<Controller>();
+            container.RegisterType<Foo>();
+
+            var controller = container.Resolve<Controller>();
+
+        }
+
+        public class Controller
+        {
+            public Controller(Func<Foo> fooCreator)
+            {
+                var foo1 = fooCreator();
+                foo1.a = 2;
+
+                var foo2 = fooCreator();
+                foo2.a = 3;
+            }
+
+        }
+
+        public class Foo
+        {
+            public int a { get; set; }
+            public Foo()
+            {
+                a = 1;
+            }
+
+        }
+           
+    }
+}
